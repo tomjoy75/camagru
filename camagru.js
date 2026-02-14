66827831d6e0792065450e80632f4b76b224f397
tomjoy75 <tomjoy75@gmail.com>
2026-02-14 17:59:17 +0100

    Add Jest test on several routes

--- camagru.js
+++ camagru.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const {engine} = require('express-handlebars');
-const port = process.env.PORT || 3000
+// const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
+const port = process.env.PORT || 3000;
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => res.render('home'));
-app.get('/about', (req, res) => res.render('about'));
-app.get('/login', (req, res) => res.render('login'));
-app.get('/register', (req, res) => res.render('register'));
+app.get('/', handlers.home);
+app.get('/about', handlers.about);
+app.get('/login', handlers.login);
+app.get('/register', handlers.register);
+
+// Custom 404 page
+app.use(handlers.notfound);
+
+// Custom 500 page
+app.use(handlers.serverError);
 
 app.listen(port, () => console.log(
 	`Express started on http://localhost:${port};` +
